f708cdc9aced72f40dd532ce5881e84448fce0f5
neringa-u <neringa.urnikyte@gmail.com>
2019-12-08 22:34:30 +0530

    hiding api_key

--- main.R
+++ main.R
@@ -1,7 +1,8 @@
 library(magrittr)
 
 list_id <- 734801183
-api_key <- "hwzb5i6w12s50molpefovr4c"
+api_key <- read.delim(file.choose(), header = F) %>% 
+  unlist %>% as.character
 list_data <-
   jsonlite::fromJSON(paste0(
     "https://openapi.etsy.com/v2/listings/",
